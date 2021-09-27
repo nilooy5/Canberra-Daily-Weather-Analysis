6a9fde01cedccc0339d38ccac06a83df001ba97b
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-27 08:45:52 +0600

    main.R added

---
+++ main.R
@@ -0,0 +1,6 @@
+# Unit & ID:    Introduction to Data Science 11516
+# Name & ID:    Fazal Mahmud Niloy 3228358
+# Description:  Intro to Data Science Assignment
+setwd(".")
+file_names <- list.files("./data")
+str(file_names)
