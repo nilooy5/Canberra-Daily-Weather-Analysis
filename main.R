ddd4c8fc5ceeac5859196e81c75c176ecb6275bb
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-27 18:04:02 +0600

    function added in main.R

--- main.R
+++ main.R
@@ -2,6 +2,26 @@
 # Name & ID:    Fazal Mahmud Niloy 3228358
 # Description:  Intro to Data Science Assignment
 setwd(".")
+library("tidyverse")
 file_names <- list.files("./data")
-temp_file <- read.csv("./data/201808.csv", skip = 7)
+
+generate_tibble <- function(filename) {
+  temp_tibble <- read_csv(
+    paste0("./data/", filename),
+    skip = 7,
+    col_types = cols(
+      Date = col_date(format = "%d/%m/%Y")
+    )
+  )
+}
+main_df <- generate_tibble(file_names[1])
+
+for (i in file_names[2:length(file_names)]) {
+  temp <- generate_tibble(i)
+  main_df <- rbind(main_df, temp)
+  print(paste("FINISHED PARSING:", i))
+}
+
+problems(main_df)
+
 str(file_names)
